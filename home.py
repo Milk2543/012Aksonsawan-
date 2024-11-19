151c3f08e15f753534b94a463028c3ca117ea772
Milk2543 <155894978+Milk2543@users.noreply.github.com>
2024-11-19 15:49:26 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,10 +4,9 @@ import pandas as pd
 st.title("Website Developing using Python")
 st.headar("Website Developing using Python")
 
-st.imge('./img/11.jpg')
+st.imge('./img/aksonsawan.jpg')
 st.subheader("Aksonsawan Deboot")
 
 dt=pd.read_csv('./data/iris-3.csv')
 st.header("ข้อมูลดอกไม้")
 st.write(dt.head(10))
-
